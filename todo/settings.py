98a15c6e4bd8b075f17041895a8121a73d05b202
R0drig <rdrpo2014@gmail.com>
2021-09-13 14:51:46 -0300

    setting.py adjustments

--- todo/settings.py
+++ todo/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9^7k1&3g9r53(&e7x87s%vshep&(y!&#g5$4)2&9l87oeb17hk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['herokudjangoapp.herokuapp.com',
                 'to-do-app-djan.herokuapp.com',
